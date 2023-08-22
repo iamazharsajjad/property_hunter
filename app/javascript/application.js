f4202eb0936fc4928ba1dbd3fc74e4e0e635464d
iamazharsajjad <iamazharsajjad@gmail.com>
2023-08-23 01:15:16 +0500

    Theme Integrated!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,21 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import 'vendor/jquery.min'
+import 'vendor/jquery-migrate-3.0.1.min'
+import 'vendor/popper.min'
+import 'vendor/bootstrap.min'
+import 'vendor/jquery.easing.1.3'
+import 'vendor/jquery.waypoints.min'
+import 'vendor/jquery.stellar.min'
+import 'vendor/owl.carousel.min'
+import 'vendor/jquery.magnific-popup.min'
+import 'vendor/aos'
+import 'vendor/jquery.animateNumber.min'
+import 'vendor/bootstrap-datepicker'
+import 'vendor/jquery.timepicker.min'
+import 'vendor/scrollax.min'
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+import 'vendor/google-map'
+import 'vendor/main'
