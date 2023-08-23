69923a02306b193e27be57d74efb3a5d29f60fbd
iamazharsajjad <iamazharsajjad@gmail.com>
2023-08-23 14:52:49 +0500

    Theme Integrated!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+import './theme/vendor/bootstrap/js/bootstrap.bundle.min'
+import './theme/vendor/swiper/swiper-bundle.min'
+import './theme/vendor/php-email-form/validate'
+
+import './theme/js/main'
